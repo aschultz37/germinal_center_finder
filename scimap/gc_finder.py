5ac53ce2d73b69caf9bf1d9403b0051ae614b5f7
Austin Schultz <aschultz6@northwell.edu>
2024-06-17 15:18:05 -0400

    helper functions for checking cell distribution

--- scimap/gc_finder.py
+++ scimap/gc_finder.py
@@ -1,6 +1,7 @@
 import scimap as sm
 import pandas as pd
 import anndata as ad
+from matplotlib.path import Path
 import math
 
 # change cell phenotypes based on list of cells
@@ -11,12 +12,40 @@ def reclassify_cells(zdata, cell_list):
          zdata.obs.loc[index, 'phenotype'] = 'False GC'
    return zdata
 
-def cell_dist(zdata, cell1, cell2):
+# calculates the Euclidian distance between two cells
+# returns distance as a float
+def cell_distance(zdata, cell1, cell2):
    cell1_x = zdata.obs.loc[cell1, 'X_centroid']
    cell1_y = zdata.obs.loc[cell1, 'Y_centroid']
    cell2_x = zdata.obs.loc[cell2, 'X_centroid']
    cell2_y = zdata.obs.loc[cell2, 'Y_centroid']
    return math.dist([cell1_x, cell1_y], [cell2_x, cell2_y])
+
+# generates points defining n equal arcs given a center point (x, y) tuple
+# returns a list of the points
+def generate_arcs(center, radius, num_arcs):
+   arcs = list()
+   angles = list(math.radians(45)*i for i in range(0, num_arcs))
+   for angle in angles:
+      x_coord = center[0] + (radius * math.cos(angle))
+      y_coord = center[1] + (radius * math.sin(angle))
+      arcs.append((x_coord, y_coord))
+   return arcs
+
+# checks if a point falls within the arc of a circle, estimated as a triangle polygon
+# returns True if within arc, False if not
+def check_within_arc(center, start_point, end_point, point_of_interest):
+   triangle = Path([center, start_point, end_point])
+   return triangle.contains_point(point_of_interest)
+
+# determines the distribution of cells in a list around a central cell within a given radius
+# returns ... something that represents that, TBD, maybe just a Boolean for if valid or not
+def cell_distribution(zdata, center_cell, invalid_cell_list, radius):
+   # break into 8 arcs
+   arcs = generate_arcs((zdata.obs.loc[center_cell, 'X_centroid'], zdata.obs.loc[center_cell, 'Y_centroid']), radius, 8)
+   # determine % invalid cells in each section
+   # if 3/8 that are primarily valid are continuous, it is OK, probably edge of GC
+   pass
 
 # determine if individual GC cell is within a B cell follicle or not
 # returns True if in follice, False if not in follicle
@@ -28,7 +57,7 @@ def cell_in_follicle(zdata, cell):
    # generate list of cells within radius of the cell of interest 
    neighborhood = list()
    for index in zdata.obs.index:
-      if cell_dist(zdata, cell, index) <= radius:
+      if cell_distance(zdata, cell, index) <= radius:
          neighborhood.append(index)
    # determine percentage of cells within radius that are not of valid type for GC definition
    invalid_cells = list()
